a8f2d807ccd363f4a9bfda253707c35035205635
Chetan Malabade <malabade.chetan@gmail.com>
2024-06-20 12:25:34 +0530

    ScheduledExecutorService programs added

---
+++ src/main/java/com/alien/SchedulingTasksWithScheduledThreadPool.java
@@ -0,0 +1,43 @@
+package com.alien;
+
+import java.util.Date;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class SchedulingTasksWithScheduledThreadPool {
+
+	public static void main(String[] args) {
+
+		System.out.println("Thread main started");
+
+		// Create a task
+		Runnable task1 = () -> {
+			System.out.println("Executing the task1 at: " + new Date());
+		};
+
+		// Create a task
+		Runnable task2 = () -> {
+			System.out.println("Executing the task2 at: " + new Date());
+		};
+
+		// Create a task
+		Runnable task3 = () -> {
+			System.out.println("Executing the task3 at: " + new Date());
+		};
+
+		ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(2);
+
+		System.out.println("Scheduling task to run after 5 seconds... " + new Date());
+
+		scheduledExecutorService.schedule(task1, 5, TimeUnit.SECONDS);
+		scheduledExecutorService.schedule(task2, 5, TimeUnit.SECONDS);
+		scheduledExecutorService.schedule(task3, 5, TimeUnit.SECONDS);
+
+		scheduledExecutorService.shutdown();
+
+		System.out.println("Thread main finished");
+
+	}
+
+}
---
+++ src/main/java/com/alien/SchedulingTasksWithScheduledThreadPoolPeriodically.java
@@ -0,0 +1,27 @@
+package com.alien;
+
+import java.util.Date;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class SchedulingTasksWithScheduledThreadPoolPeriodically {
+
+	public static void main(String[] args) {
+
+		System.out.println("Thread main started");
+
+		ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(1);
+
+		// Create a task
+		Runnable task1 = () -> {
+			System.out.println("Executing the task1 at: " + new Date());
+		};
+
+		scheduledExecutorService.scheduleAtFixedRate(task1, 0, 2, TimeUnit.SECONDS);
+
+		System.out.println("Thread main finished");
+
+	}
+
+}
